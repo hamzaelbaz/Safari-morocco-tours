4bc20cde9539796e9fb787fad8b4c062eb1f05d9
hamza <elbazhamza77@gmail.com>
2025-05-31 17:40:53 +0200

    Initial commit

---
+++ public/js/form.js
@@ -0,0 +1,25 @@
+const form = document.querySelector("form"),
+    firstName = document.querySelector("#first-name"),
+    lastName = document.querySelector("#last-name"),
+    email = document.querySelector("#email"),
+    message = document.querySelector("#message"),
+    tel = document.querySelector('#tel')
+form.addEventListener("submit", async e => {
+    e.preventDefault();
+    let a = firstName.value,
+        r = lastName.value,
+        n = email.value,
+        o = message.value,
+        p = tel.value;
+    try {
+        await axios.post("/.netlify/functions/sendEmail", {
+            firstNames: a,
+            lastNames: r,
+            emails: n,
+            messages: o,
+            tele: p
+        }), firstName.value = "", lastName.value = "", email.value = "", message.value = "", tel.value =""
+    } catch (s) {
+        console.error("oups! elle y a une erreur:", s)
+    }
+});
